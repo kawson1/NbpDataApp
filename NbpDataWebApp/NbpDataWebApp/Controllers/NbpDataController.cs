a7c16c149640023bbe04f322e091ad6f9b5374fd
kawson <piegusek20@gmail.com>
2023-04-25 01:05:07 +0200

    New actions in NbpDataController.cs - buyselldiff endpoint - shows data about major difference between buy/sell
    - exchanges/{code}/{count} for min and max exchange values

--- NbpDataWebApp/NbpDataWebApp/Controllers/NbpDataController.cs
+++ NbpDataWebApp/NbpDataWebApp/Controllers/NbpDataController.cs
@@ -8,19 +8,65 @@ namespace NbpDataWebApp.Controllers;
 [Route("NbpData")]
 public class NbpDataController : Controller
 {
+    // TODO: handle if there is no data for specific day
+    // TODO: handle if exchangeDate is correct format (yyyy-mm-dd)
     [HttpGet]
-    [Route("exchanges/{exchangeDate}")]
-    public async Task<string> Exchange(string exchangeDate)
+    [Route("exchanges/{currencyCode}/{exchangeDate:datetime}")]
+    public async Task<string> Exchange(string currencyCode, string exchangeDate)
     {
-        Console.WriteLine(exchangeDate);
         using (HttpClient httpClient = new HttpClient())
         {
-            var response = await httpClient.GetAsync("http://api.nbp.pl/api/exchangerates/rates/a/AUD/today/");
+            var response = await httpClient.GetAsync($"http://api.nbp.pl/api/exchangerates/rates/a/{currencyCode}/{exchangeDate}/");
             if (response.IsSuccessStatusCode)
             {
                 var responseString = await response.Content.ReadAsStringAsync();
                 ExchangeData ex = ExchangeDataModel.GetDataFromJson(responseString);
                 return $"Exchange code: {ex.currencyCode}\nAverage exchange rate: {ex.exchangeRate}";
+            }
+            else
+                return $"Couldn't get data";
+        }
+    }
+    
+    [HttpGet]
+    [Route("exchanges/{currencyCode}/{count:int}")]
+    public async Task<string> MinMaxExchange(string currencyCode, int count)
+    {
+        using (HttpClient httpClient = new HttpClient())
+        {
+            var response = await httpClient.GetAsync($"http://api.nbp.pl/api/exchangerates/rates/a/{currencyCode}/last/{count}/");
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var (min, max) = ExchangeDataModel.GetMinMaxFromJson(responseString);
+                return $"Minimal exchange data for {currencyCode}:\n\t" +
+                       $"Effective data: {min.effectiveDate.ToShortDateString()}\n\t" +
+                       $"Exchange rate: {min.exchangeRate}\n" +
+                       
+                       $"Maximal exchange data for {currencyCode}:\n\t" +
+                       $"Effective data: {max.effectiveDate.ToShortDateString()}\n\t" +
+                       $"Exchange rate: {max.exchangeRate}";
+            }
+            else
+                return $"Couldn't get data";
+        }
+    }
+    
+    [HttpGet]
+    [Route("buyselldiff/{currencyCode}/{count:int}")]
+    public async Task<string> BiggestBuySellDifference(string currencyCode, int count)
+    {
+        using (HttpClient httpClient = new HttpClient())
+        {
+            var response = await httpClient.GetAsync($"http://api.nbp.pl/api/exchangerates/rates/c/{currencyCode}/last/{count}/");
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                ExchangeData exchange = ExchangeDataModel.GetBuySellDiffFromJson(responseString);
+                return $"Major difference between buy & sell: {exchange.ask - exchange.bid}:\n\t" +
+                       $"Effective data: {exchange.effectiveDate.ToShortDateString()}\n\t" +
+                       $"Exchange bid: {exchange.bid}\n\t" +
+                       $"Exchange ask: {exchange.ask}";
             }
             else
                 return $"Couldn't get data";
--- NbpDataWebApp/NbpDataWebApp/Models/ExchangeData.cs
+++ NbpDataWebApp/NbpDataWebApp/Models/ExchangeData.cs
@@ -3,10 +3,14 @@ using Newtonsoft.Json.Linq;
 
 namespace NbpDataWebApp.Models;
 
+// TODO: merge this object into smaller DTO objects
 public class ExchangeData
 { 
     public string currencyCode;
-    public float exchangeRate; 
+    public float exchangeRate;
+    public float bid;
+    public float ask;
+    public DateTime effectiveDate;
 }
 
 public static class ExchangeDataModel
@@ -25,6 +29,99 @@ public static class ExchangeDataModel
             };
 
             return ex;
+        }
+        // Catch JSON parse error
+        catch (JsonReaderException ex)
+        {
+            Console.WriteLine($"JSON parse error: {ex.Message}");
+        }
+        
+        // Catch float parse error
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Float parse error: {ex.Message}");
+        }
+        
+        // Other exception
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unexpected error: {ex.Message}");
+        }
+
+        return null;
+    }
+    
+    /// <summary>
+    /// Returns ExchangeData tuple - minimal exchange value object and maximum exchange value object
+    /// </summary>
+    /// <param name="jsonString"></param>
+    /// <returns></returns>
+    public static (ExchangeData Min, ExchangeData Max) GetMinMaxFromJson(string jsonString)
+    {
+        try
+        {
+            var jsonObject = JObject.Parse(jsonString);
+            var rates = jsonObject.SelectToken("rates");
+            
+            Console.WriteLine(rates);
+            List<ExchangeData> exchanges = 
+                rates.AsEnumerable()
+                    .Select(rate =>
+            {
+                return new ExchangeData
+                {
+                    currencyCode = "AUD",
+                    exchangeRate = float.Parse(rate.SelectToken("mid").ToString()),
+                    effectiveDate = DateTime.Parse(rate.SelectToken("effectiveDate").ToString())
+                };
+            })
+                    .ToList();
+            ExchangeData min = exchanges.MinBy(exchange => exchange.exchangeRate);
+            ExchangeData max = exchanges.MaxBy(exchange => exchange.exchangeRate);
+            return (min, max);
+        }
+        // Catch JSON parse error
+        catch (JsonReaderException ex)
+        {
+            Console.WriteLine($"JSON parse error: {ex.Message}");
+        }
+        
+        // Catch float parse error
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Float parse error: {ex.Message}");
+        }
+        
+        // Other exception
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unexpected error: {ex.Message}");
+        }
+
+        return (null, null);
+    }
+    
+    public static ExchangeData GetBuySellDiffFromJson(string jsonString)
+    {
+        try
+        {
+            var jsonObject = JObject.Parse(jsonString);
+            var rates = jsonObject.SelectToken("rates");
+            
+            Console.WriteLine(rates);
+            List<ExchangeData> exchanges = 
+                rates.AsEnumerable()
+                    .Select(rate =>
+            {
+                return new ExchangeData
+                {
+                    effectiveDate = DateTime.Parse(rate.SelectToken("effectiveDate").ToString()),
+                    bid = float.Parse(rate.SelectToken("bid").ToString()),
+                    ask = float.Parse(rate.SelectToken("ask").ToString())
+                };
+            })
+                    .ToList();
+            return exchanges.MaxBy(exchange => exchange.ask - exchange.bid);
         }
         // Catch JSON parse error
         catch (JsonReaderException ex)
